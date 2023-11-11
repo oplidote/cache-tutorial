985d8332fb65317e44f8839ca3e93e8d85949a1b
jhwmac <jkr2505@naver.com>
2023-11-11 15:37:33 +0900

    chore: install tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="ko">
+      <head>
+        <title>캐시 튜토리얼</title>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
